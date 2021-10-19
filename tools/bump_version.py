6d437a80afe57f2efb9e6279a9c66cfcc4841454
Craig Gumbley <craiggumbley@gmail.com>
2021-10-19 17:03:44 +0100

    refactor

--- tools/bump_version.py
+++ tools/bump_version.py
@@ -1,33 +1,36 @@
 import toml
-import sys
 import os
 from pkg_resources import parse_version
 
+# Exception: The ref refs/pull/55/merge is not valid.
 
-def bump_version_from_ref():
+
+def bump_version_from_ref() -> None:
     """Bumps the version property inside pyproject.toml."""
 
-    ref = os.environ.get("GITHUB_REF", "v0.0.0")
-    ref_suffix = ref.split("/")[-1]
-    if "tags" not in ref or not ref_suffix.startswith("v"):
-        raise Exception(f"The ref {ref} is not valid.")
+    ref = os.environ.get("GITHUB_REF", "refs/is/invalid")
 
+    if not ref.startswith("refs/tags/v"):
+        print(f"The given ref didn't match the requirements for a version increment.")
+        return
+
+    ref_suffix = ref.split("/")
     project_file = "pyproject.toml"
     project = toml.load(project_file)
 
-    next_version = ref_suffix[1:]
+    next_version = str(ref_suffix[1:])
     current_version = project["tool"]["poetry"]["version"]
     is_next_version = parse_version(next_version) > parse_version(current_version)
 
-    if is_next_version:
-        print(f"⭐ Next version will be: {next_version}")
-        project["tool"]["poetry"]["version"] = next_version
-        with open(project_file, "w") as f:
-            toml.dump(project, f)
-    else:
-        print(
-            f"✨ No version bump needed. Next version was {next_version} and current version is {current_version}."
-        )
+    if not is_next_version:
+        f"✨ No version bump needed. Next version was {next_version} and current version is {current_version}."
+        return
+
+    print(f"⭐ Next version will be: {next_version}")
+    project["tool"]["poetry"]["version"] = next_version
+
+    with open(project_file, "w") as f:
+        toml.dump(project, f)
 
 
 if __name__ == "__main__":
