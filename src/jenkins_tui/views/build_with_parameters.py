4b4aca4afcffe6f2bc0b5c89cdc4698f15b83b98
Craig Gumbley <craiggumbley@gmail.com>
2021-11-20 21:13:20 +0000

    clean up default parameters and focus on first field on show

--- src/jenkins_tui/views/build_with_parameters.py
+++ src/jenkins_tui/views/build_with_parameters.py
@@ -43,6 +43,9 @@ class BuildWithParametersView(BaseView):
         self.fields: list[Widget] = []
         self.current_button: ButtonWidget | None = None
 
+    async def on_show(self):
+        await self.app.set_focus(self.fields[0])
+
     async def on_mount(self) -> None:
         """Actions that are executed when the widget is mounted."""
 
@@ -59,11 +62,17 @@ class BuildWithParametersView(BaseView):
                 parameter["description"] or "", style=Style(italic=True, dim=True)
             )
 
+            default_parameter = (
+                parameter["defaultParameterValue"].get("value", "")
+                if parameter["defaultParameterValue"]
+                else ""
+            )
+
             field = TextInputFieldWidget(
                 name=name,
                 title=title,
                 placeholder=placeholder,
-                default_value=parameter["defaultParameterValue"].get("value"),
+                default_value=default_parameter,
                 choices=parameter.get("choices", []),
                 border_style="medium_purple4"
                 # validation_regex=parameter.get("validationRegex", None),
@@ -74,7 +83,6 @@ class BuildWithParametersView(BaseView):
 
         self.layout.place(*self.fields)
 
-        # Buttons
         self.layout.add_row("row_buttons", size=5)
 
         await self.add_button(text="ok")
@@ -85,16 +93,18 @@ class BuildWithParametersView(BaseView):
         buttons_view.grid.add_column("cancel", size=15)
         buttons_view.grid.add_row("row", size=3)
         buttons_view.grid.set_align("center", "center")
-        # buttons_view.grid.set_gutter(0, 0)
         buttons_view.grid.place(*list(self.buttons.values()))
 
         self.layout.place(*[buttons_view])
 
+        # set focus on first text input field
         await self.refresh_layout()
 
     async def handle_button_pressed(self, message: ButtonPressed):
         # reset previous current button toggle value
         self.log(f"Handling button press: {message.name}")
+        message.stop()
+
         if self.current_button:
             self.current_button.toggle = False
 
@@ -102,6 +112,9 @@ class BuildWithParametersView(BaseView):
         self.current_button = message.sender
         assert isinstance(self.current_button, ButtonWidget)
         self.current_button.toggle = True
+
+        if message.sender.name == "cancel":
+            await self.parent.action("history")
 
         if message.sender.name == "ok":
 
@@ -113,8 +126,6 @@ class BuildWithParametersView(BaseView):
                 if not valid:
                     validation_failed = True
                     self.current_button.toggle = False
-
-            message.stop()
 
             if not validation_failed:
                 field.value = field.default_value or ""
@@ -135,8 +146,6 @@ class BuildWithParametersView(BaseView):
                         )
                     )
 
-                await self.post_message_from_child(
-                    ButtonPressed(self.parent.nav.buttons["history"])
-                )
+                await self.parent.action("history")
 
-        self.refresh()
+        self.refresh(layout=True)
