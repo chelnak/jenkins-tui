8a4c5598de242c8bd29979f61f0bc1d1284ab9be
Craig Gumbley <craiggumbley@gmail.com>
2021-11-24 15:04:54 +0000

    improve widget switching

--- src/jenkins_tui/app.py
+++ src/jenkins_tui/app.py
@@ -1,17 +1,15 @@
-import os
 import sys
 from io import TextIOWrapper
 from typing import Optional
 
 import click
 from click.types import File
-from rich.text import Text
 from textual.app import App
 from textual.keys import Keys
 from textual.reactive import Reactive
 from textual.widgets import NodeID, TreeNode
 
-from . import __version__, styles
+from . import __version__
 from .config import APP_NAME, CLI_HELP, get_config
 from .containers import Container
 from .views import CustomScrollView, HomeView, SideBarView
@@ -31,38 +29,14 @@ class JenkinsTUI(App):
     search_node: Reactive[int] = Reactive(0)
     searchable_nodes: Reactive[dict[NodeID, TreeNode]] = Reactive({})
     show_help = Reactive(False)
-    show_command_pallet = Reactive(False)
+    show_search = Reactive(False)
     nav_title: Reactive[str] = Reactive("")
 
     async def on_load(self) -> None:
         """Overrides on_load from App()"""
         await self.bind("?", "toggle_help", "show help")
         await self.bind(Keys.Escape, "refocus_tree", show=False)
-        await self.bind(Keys.ControlK, "toggle_command_pallet", show=False)
-
-    async def watch_show_help(self, show_help: bool) -> None:
-        self.help.visible = show_help
-
-    async def action_toggle_help(self) -> None:
-        self.show_help = not self.show_help
-
-    async def watch_show_command_pallet(self, show_command_pallet: bool) -> None:
-        self.command_pallet.visible = show_command_pallet
-
-    async def action_toggle_command_pallet(self) -> None:
-        self.show_command_pallet = not self.show_command_pallet
-
-        if self.show_command_pallet:
-            await self.app.set_focus(self.command_pallet)
-        else:
-            await self.action_refocus_tree()
-
-    async def action_refocus_tree(self) -> None:
-        """Actions that are executed when the history button is pressed."""
-        self.show_help = False
-        self.show_command_pallet = False
-        self.command_pallet.value = ""
-        await self.side_bar.set_tree_focus()
+        await self.bind(Keys.ControlK, "toggle_search", show=False)
 
     async def on_mount(self) -> None:
         """Overrides on_mount from App()"""
@@ -73,13 +47,9 @@ class JenkinsTUI(App):
         self.nav = NavWidget()
         await self.view.dock(self.nav, edge="top", size=8, name="nav")
 
-        self.command_pallet = SearchWidget()
-        self.command_pallet.visible = False
-        await self.view.dock(
-            self.command_pallet, edge="top", size=3, name="command_pallet"
-        )
+        self.search = SearchWidget()
+        await self.view.dock(self.search, edge="top", size=3, name="search")
 
-        # Dock content container
         self.container = CustomScrollView(
             intial_view=HomeView(), name="ContentScrollView"
         )
@@ -90,9 +60,40 @@ class JenkinsTUI(App):
         await self.view.dock(self.flash, edge="bottom", z=1)
 
         self.help = HelpWidget()
-        self.help.visible = False
-
         await self.view.dock(self.help, edge="left", size=40, z=1)
+
+    async def watch_show_help(self, show_help: bool) -> None:
+        self.help.visible = show_help
+
+    async def watch_show_search(self, show_search: bool) -> None:
+        self.search.visible = show_search
+
+    async def action_toggle_help(self) -> None:
+        self.show_help = not self.show_help
+
+        if self.show_help:
+            await self.app.set_focus(self.help)
+        else:
+            await self.side_bar.set_tree_focus()
+
+    async def action_toggle_search(self) -> None:
+        self.show_search = not self.show_search
+
+        if self.show_search:
+            await self.app.set_focus(self.search)
+        else:
+            await self.side_bar.set_tree_focus()
+
+    async def action_refocus_tree(self) -> None:
+
+        if self.side_bar.tree_has_focus:
+            await self.side_bar.reset_tree()
+        else:
+            self.show_help = False
+            self.show_search = False
+            self.search.value = ""
+
+            await self.side_bar.set_tree_focus()
 
     async def handle_show_flash_notification(self, message: ShowFlashNotification):
         self.log("Handling ShowFlashNotification message")
