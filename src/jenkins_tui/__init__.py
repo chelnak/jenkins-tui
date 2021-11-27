7890825d08700f028d44d8552c3e03c0436e90b8
Craig Gumbley <craiggumbley@gmail.com>
2021-11-27 09:17:40 +0000

    Use pylev to remove dep on c

--- src/jenkins_tui/__init__.py
+++ src/jenkins_tui/__init__.py
@@ -1,3 +1,7 @@
 import importlib.metadata
+import sys
+
+if (sys.version_info[0], sys.version_info[1]) < (3, 9):
+    sys.exit("fast-autocomplete requires Python 3.9 or later.")
 
 __version__ = importlib.metadata.version("jenkins_tui")
